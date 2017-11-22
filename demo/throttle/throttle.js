f32c16f11406ca297ac01a7194d29c65445fbe84
左政 <zz43002@ly.com>
2017-11-22 10:56:46 +0800

    'JavaScript-函数防抖和节流'

--- demo/throttle/throttle.js
+++ demo/throttle/throttle.js
@@ -20,35 +20,42 @@
 					timeout = null;
 					//函数放在此处执行，和放在外面(2)执行区别是这里在刚进入时会延迟一个wait执行
 					//并且在停止触发wait时间后会再触发一次
-					func.apply(context,args);
+					//func.apply(context,args);
 				},wait);
 				//2
-				//func.apply(context,args);
+				func.apply(context,args);
 			}
 		}
 	}
 
 	//升级版：兼顾进入时触发处理事件，且在停止触发一个wait时间后再触发处理事件
 	function throttle(func, wait){
-		var timeout,isFirst = true;
+		//
+		var timeout,starttime = 0;
 		return function(){
+
 			var context = this, args = arguments;
-			if(!timeout){
-				timeout = setTimeout(function(){
-					timeout = null; 
-					if(!isFirst){
-						func.apply(context,args);
-					}
-				});
-				if(isFirst){
-					func.apply(context,args);
+
+			var nowtime = Date.now();
+
+			var remaining = wait - (nowtime - starttime);
+
+			if(remaining <= 0){
+				if(timeout){
+					clearTimeout(timeout);
+					timeout = null;
 				}
-			}
+				starttime = Date.now();
+				func.apply(context,args); 
+			}else if(!timeout){
+				timeout = setTimeout(function(){
+					timeout = null;  
+					func.apply(context,args); 
+					starttime = Date.now();
+				},remaining);
+			} 
 		}
-	}
-
-
-
+	} 
 	$.throttle =  {
 		timestampthrottle,
 		timesoutthrottle,
