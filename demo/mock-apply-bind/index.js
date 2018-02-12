c24599ee9b78cc557052d164670cea492561e8d5
左政 <zz43002@ly.com>
2018-02-12 15:16:43 +0800

    ### callnew、applynew、bindnew 代码

--- demo/mock-apply-bind/index.js
+++ demo/mock-apply-bind/index.js
@@ -1,61 +1,100 @@
-(function(global){
-	Function.prototype.applynew = function(thisArg,args){ 
+(function(global) {
 
-		args = args || [];
+    function capotalize(str) {
+        var classArry = {};
+        "Boolean Number String".split(" ").forEach(function(item) {
+            classArry[item.toLowerCase()] = item;
+        });
+        return classArry[str];
+    }
 
-		const capotalize = ([first,...rest]) =>{
-			return first.toUpperCase() + rest.join('');
-		};
+    Function.prototype.callnew = Function.prototype.callnew || function(context) {
+        var contextClass = capotalize(typeof context);
+        if (contextClass) {
+            context = eval('new ' + contextClass + '(context)');
+        } else {
+            context = context || window;
+        }
 
-		let objtype = capotalize(typeof thisArg);
+        var args = [],
+            result;
+        context._fn_ = context._fn_ || this;
+        if (arguments.length > 1) {
+            for (var i = 1; i < arguments.length; i++) {
+                args.push('arguments[' + i + ']');
+            }
+            result = eval('context._fn_(' + args + ')');
+        } else {
+            result = context._fn_();
+        }
+        delete context._fn_;
+        return result;
+    }
 
-		if(objtype !== 'Object' && objtype !=='Undefined' ){
-			thisArg = eval('new ' + objtype + '('+ thisArg +')');
-		}else{
-			thisArg = thisArg || global;
-		}
 
-		var fn = Symbol('callback'); 
+    Function.prototype.applynew = Function.prototype.applynew || function(context, arry) {
+        var contextClass = capotalize(typeof context);
+        if (contextClass) {
+            context = eval('new ' + contextClass + '(context)');
+        } else {
+            context = context || window;
+        }
+        var result, args = [];
+        context._fn_ = context._fn_ || this;
+        if (arry) {
+            for (var i = 0; i < arry.length; i++) {
+                args.push('arry[' + i + ']');
+            }
+            result = eval('context._fn_(' + args + ')');
 
-		thisArg[fn] = this;
+        } else {
+            result = context._fn_();
+        }
+        delete context._fn_;
+        return result;
+    }
 
-		var returnValue = thisArg[fn](...args);
+    Function.prototype.bindnew = Function.prototype.bindnew || function(context) {
+        var self = this;
+        var args = Array.prototype.slice.call(arguments, 1);
+        var bindFun = function() {
+            var bindArgs = Array.prototype.slice.call(arguments);
+            return self.apply(this instanceof bindFun ? this : context, args.concat(bindArgs));
+        };
 
-		delete thisArg[fn];
+        function fNOP() {} 
+        fNOP.prototype = this.prototype; 
+        bindFun.prototype = new fNOP();
 
-		return returnValue;
-	}; 
+        //bindFun.prototype = Object(this.prototype);
 
-	Function.prototype.bindnew = function(obj){
-		var fn = this;
-		return function(){
-			return fn.applynew(obj,arguments);
-		}
-	};
+        return bindFun;
+    }
+
 })(window);
 
 
 var name = 'global scope';
 
 var obj1 = {
-	name : 'xiaodaidai'
+    name: 'xiaodaidai'
 };
 
 var obj2 = {
-	name : 'xiaopangzi'
+    name: 'xiaopangzi'
 }
 
 var $wrapper = document.querySelector('.wrapper');
 
-function testApply(){  
-	$wrapper.innerHTML = $wrapper.innerHTML + '\n'  + this +  this.name ;
+function testApply() {
+    $wrapper.innerHTML = $wrapper.innerHTML + '\n' + this + this.name;
 }
 
-function foo(name,age){
+function foo(name, age) {
     console.log(name + age);
     console.log(this);
-    console.log(this.toString()); 
-    $wrapper.innerHTML = $wrapper.innerHTML + '\n'  + name + age +  this + this.toString() ;
+    console.log(this.toString());
+    $wrapper.innerHTML = $wrapper.innerHTML + '\n' + name + age + this + this.toString();
     return name;
 }
 
@@ -63,7 +102,7 @@ function foo(name,age){
 
 testApply.applynew();
 
-var returnName = foo.applynew(123,['xiaodaidai',18]);
+var returnName = foo.applynew(123, ['xiaodaidai', 18]);
 
 console.log(returnName);
 
@@ -71,8 +110,8 @@ testApply.applynew(obj1);
 
 testApply.applynew(obj2);
 
-function bindtest(){
-	 $wrapper.innerHTML = $wrapper.innerHTML + '\n'  + this.name;
+function bindtest() {
+    $wrapper.innerHTML = $wrapper.innerHTML + '\n' + this.name;
 }
 bindtest();
 var bar = bindtest.bindnew(obj1);
